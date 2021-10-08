82808ccc6e2b14a4320fa52356c63137855f56f2
zakissimo <zakaria.habri@gmail.com>
2021-10-09 00:59:42 +0200

    teaks

--- desktop/qtile/.config/qtile/config.py
+++ desktop/qtile/.config/qtile/config.py
@@ -17,8 +17,8 @@ colors = []
 cache = '/home/zak/.cache/wal/colors'
 
 
-def load_colors(cache):
-    with open(cache, 'r') as file:
+def load_colors(c):
+    with open(c, 'r', encoding="UTF-8") as file:
         for line in file:
             colors.append(line.strip())
 
@@ -28,6 +28,7 @@ def load_colors(cache):
 
 
 load_colors(cache)
+
 
 keys = [
 
@@ -296,13 +297,3 @@ def start_once():
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
 auto_minimize = True
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
-wmname = "LG3D"
