7bd11195f16ef8db9fd424ed2fda08f0ada6c264
zakissimo <zakaria.habri@gmail.com>
2021-10-12 20:55:10 +0200

    kek

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -49,6 +49,8 @@
          '(
            (".*Noto Naskh Arabic.*" . 1.7)))
 
+(setq-default default-input-method "arabic")
+
 (global-unset-key (kbd "M-SPC"))
 (global-set-key (kbd "M-SPC") 'toggle-input-method)
 
