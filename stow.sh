9b35d9d800bdb35c91424bac9fb541908faf5b1a
zakissimo <zakaria.habri@gmail.com>
2021-11-19 22:03:14 +0100

    kek

--- stow.sh
+++ stow.sh
@@ -10,13 +10,13 @@ plateform=("laptop" "desktop" "quit")
 select choice in "${plateform[@]}"; do
     case $choice in
         desktop)
-            cd "$HOME"/.dotfiles && stow -vSt "$HOME" qtile || echo "Can't cd in dotfiles' folder! Failed stowing config.py!"
-            cd "$HOME"/.dotfiles/desktop && stow -vSt "$HOME" qtile || echo "Can't cd in dotfiles' folder! Failed stowing qtile scripts!"
+            cd "$HOME"/.dotfiles && stow -vSt "$HOME" qtile || echo "Failed stowing config.py!"
+            cd "$HOME"/.dotfiles/desktop && stow -vSt "$HOME" qtile || echo "Failed stowing qtile scripts!"
             break
             ;;
         laptop)
-            cd "$HOME"/.dotfiles && stow -vSt "$HOME" qtile || echo "Can't cd in dotfiles' folder! Failed stowing config.py & autostart.sh!"
-            cd "$HOME"/.dotfiles/laptop && stow -vSt "$HOME" qtile || echo "Can't cd in dotfiles' folder! Failed stowing qtile scripts!"
+            cd "$HOME"/.dotfiles && stow -vSt "$HOME" qtile || echo "Failed stowing config.py & autostart.sh!"
+            cd "$HOME"/.dotfiles/laptop && stow -vSt "$HOME" qtile || echo "Failed stowing qtile scripts!"
             break
             ;;
         quit)
@@ -31,21 +31,21 @@ done
 
 [ ! -d "$HOME"/.config/picom ] && mkdir -p "$HOME"/.config/picom
 [ -f "$HOME"/.config/qtile/picom.conf ] && rm "$HOME"/.config/qtile/picom.conf
-cd "$HOME"/.dotfiles && stow -vSt "$HOME" picom || echo "Can't cd in dotfiles' folder! Failed stowing!"
+cd "$HOME"/.dotfiles && stow -vSt "$HOME" picom || echo "Failed stowing!"
 
 [ ! -d "$HOME"/.config/kitty ] && mkdir -p "$HOME"/.config/kitty
 [ -f "$HOME"/.config/kitty/kitty.conf ] && rm "$HOME"/.config/kitty/kitty.conf
-cd "$HOME"/.dotfiles && stow -vSt "$HOME" kitty || echo "Can't cd in dotfiles' folder! Failed stowing!"
+cd "$HOME"/.dotfiles && stow -vSt "$HOME" kitty || echo "Failed stowing!"
 
-[ ! -d "$HOME"/.config/sxiv/exec ] && mkdir -p "$HOME"/.config/sxiv/exec && touch
+[ ! -d "$HOME"/.config/sxiv/exec ] && mkdir -p "$HOME"/.config/sxiv/exec
 [ -f "$HOME"/.config/sxiv/exec/key-handler ] && rm "$HOME"/.config/sxiv/exec/key-handler
-cd "$HOME"/.dotfiles && stow -vSt "$HOME" sxiv || echo "Can't cd in dotfiles' folder! Failed stowing!"
+cd "$HOME"/.dotfiles && stow -vSt "$HOME" sxiv || echo "Failed stowing!"
 
 [ ! -d "$HOME"/.config/neovim ] && mkdir -p "$HOME"/.config/nvim
 [ -f "$HOME"/.config/nvim/init.vim ] && rm "$HOME"/.config/nvim/init.vim
-cd "$HOME"/.dotfiles && stow -vSt "$HOME" neovim || echo "Can't cd in dotfiles' folder! Failed stowing!"
+cd "$HOME"/.dotfiles && stow -vSt "$HOME" neovim || echo "Failed stowing!"
 
-cd "$HOME"/.dotfiles && stow -vSt "$HOME" starship || echo "Can't cd in dotfiles' folder! Failed stowing!"
+cd "$HOME"/.dotfiles && stow -vSt "$HOME" starship || echo "Failed stowing!"
 
 rm "$HOME"/.zshrc
-cd "$HOME"/.dotfiles && stow -vSt "$HOME" zsh || echo "Can't cd in dotfiles' folder! Failed stowing!"
+cd "$HOME"/.dotfiles && stow -vSt "$HOME" zsh || echo "Failed stowing!"
