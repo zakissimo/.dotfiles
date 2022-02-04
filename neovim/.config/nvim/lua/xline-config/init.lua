f648b003a46ce30e7b778f53239e4959f52759ba
zakissimo <zakaria.habri@gmail.com>
2022-02-04 20:42:01 +0100

    Upgrading Lsp settings

--- neovim/.config/nvim/lua/xline-config/init.lua
+++ neovim/.config/nvim/lua/xline-config/init.lua
@@ -1,12 +1,7 @@
--- local cmd = vim.cmd
-
-require'lualine'.setup({
-    options = {
-		theme = 'catppuccin',
-        section_separators = { left = '', right = ''},
-        component_separators = '',
-    },
+require("lualine").setup({
+	options = {
+		theme = "catppuccin",
+		section_separators = { left = "", right = "" },
+		component_separators = "",
+	},
 })
-
--- cmd[[hi NvimTreeStatusLineNC guibg=nvim_treebg guifg=nvim_treebg]]
--- cmd[[au BufEnter,BufWinEnter,WinEnter,CmdwinEnter * if bufname('%') == "NvimTree" || bufname('%') == "packer" | set laststatus=0 | else | set laststatus=2 | endif]]
