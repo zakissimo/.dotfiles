058b0c895b9f29b9546a2e4881eff27833e2b600
zakissimo <zakaria.habri@gmail.com>
2022-02-04 21:31:02 +0100

    lel

--- neovim/.config/nvim/lua/indent-config/init.lua
+++ neovim/.config/nvim/lua/indent-config/init.lua
@@ -1,13 +1,13 @@
 vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#6E6C7E gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#6E6C7E gui=nocombine]]
 
-require("indent_blankline").setup {
-    char = "│",
-    char_highlight_list = {
-        "IndentBlanklineIndent1",
-    },
-    space_char_highlight_list = {
-        "IndentBlanklineIndent1",
-    },
-    show_trailing_blankline_indent = false,
-}
+require("indent_blankline").setup({
+	char = "│",
+	-- char_highlight_list = {
+	--     "IndentBlanklineIndent1",
+	-- },
+	-- space_char_highlight_list = {
+	--     "IndentBlanklineIndent1",
+	-- },
+	show_trailing_blankline_indent = false,
+})
