d742977de1e8a2d58b689aa809690948eb0a465b
zakissimo <zakaria.habri@gmail.com>
2022-02-05 15:27:34 +0100

    Correcting Pacman dl pbs

--- InstallationScripts/deploy.sh
+++ InstallationScripts/deploy.sh
@@ -7,12 +7,9 @@ git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 cd .. && sudo rm -rf yay
 
-yay -S picom-jonaburg-git megasync-bin brave-bin nerd-fonts-cascadia-code ttf-impallari-lobster-font lxappearance xflux notion-app-enhanced
+yay -S picom-jonaburg-git nvim-packer-git megasync-bin brave-bin nerd-fonts-cascadia-code ttf-impallari-lobster-font
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-sh -c "$(curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
