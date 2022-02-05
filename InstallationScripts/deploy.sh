e4bf4bef2e54f6797aa717dc30ce5a92d1c65124
zakissimo <zakaria.habri@gmail.com>
2022-02-05 23:51:59 +0100

    seperated zsh plugins and keyboard config

--- InstallationScripts/deploy.sh
+++ InstallationScripts/deploy.sh
@@ -16,19 +16,3 @@ sudo pip install pynvim requests black autopep8 pylint
 sudo npm install neovim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-# echo "#########################################################"
-# echo "## Installing Doom Emacs. This may take a few minutes. ##"
-# echo "#########################################################"
-# [ -d ~/.emacs.d ] && mv ~/.emacs.d ~/.emacs.d.bak
-# [ -f ~/.emacs ] && mv ~/.emacs ~/.emacs.bak
-# git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-# ~/.emacs.d/bin/doom install
-
-#sudo cp "./keyboard/arabic.el" "/usr/share/emacs/27.2/lisp/leim/quail/"
-#sudo cp "./keyboard/fr" "/usr/share/X11/xkb/symbols/"
-#sudo cp "./keyboard/ara" "/usr/share/X11/xkb/symbols/"
