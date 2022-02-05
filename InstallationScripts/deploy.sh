e7bbc47fdac55a9d5535aee5fc2e729597cc72c9
zakissimo <zakaria.habri@gmail.com>
2022-02-05 17:57:54 +0100

    update deploy and pkglist

--- InstallationScripts/deploy.sh
+++ InstallationScripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sudo pacman --needed --ask 4 -Sy - < pkglist.txt
+
 echo "#########################################################"
 echo "## ............... Installing yay .................... ##"
 echo "#########################################################"
@@ -7,7 +9,11 @@ git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 cd .. && sudo rm -rf yay
 
-yay -S picom-jonaburg-git megasync-bin brave-bin nerd-fonts-cascadia-code ttf-impallari-lobster-font
+yay -S picom-jonaburg-git megasync-bin brave-bin nerd-fonts-cascadia-code ttf-impallari-lobster-font visual-studio-code-bin
+
+sudo pip install pynvim
+
+sudo npm install neovim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
