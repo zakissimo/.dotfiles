51b34cde2e0c13d93c92dc73b98dc80bb0a234d9
zakissimo <zakaria.habri@gmail.com>
2022-02-08 23:27:44 +0100

    update

--- InstallationScripts/BraveSync.sh
+++ InstallationScripts/BraveSync.sh
@@ -5,13 +5,13 @@ command -v brave >/dev/null 2>&1 || { echo >&2 "I require brave but it's not ins
 command -v megasync >/dev/null 2>&1 || { echo >&2 "I require megasync but it's not installed. Aborting."; exit 1; }
 
 import(){
-    [ ! -d "$HOME/MEGA/linuxrc/BraveSettings" ] && { echo >&2 "No saved settings found in MEGA! Aborting."; exit 1; }
-    cp -r "$HOME/MEGA/linuxrc/BraveSettings/Default" "$HOME/.config/BraveSoftware/Brave-Browser/" && echo "Settings successfully synchronised!"
+    [ ! -d "$HOME/MEGA/Linuxrc/BraveSettings" ] && { echo >&2 "No saved settings found in MEGA! Aborting."; exit 1; }
+    cp -r "$HOME/MEGA/Linuxrc/BraveSettings/Default" "$HOME/.config/BraveSoftware/Brave-Browser/" && echo "Settings successfully synchronised!"
 }
 
 export(){
-    [ ! -d "$HOME/MEGA/linuxrc/BraveSettings" ] && mkdir -p "$HOME/MEGA/linuxrc/BraveSettings"
-    cp -r "$HOME/.config/BraveSoftware/Brave-Browser/Default" "$HOME/MEGA/linuxrc/BraveSettings/"
+    [ ! -d "$HOME/MEGA/Linuxrc/BraveSettings" ] && mkdir -p "$HOME/MEGA/Linuxrc/BraveSettings"
+    cp -r "$HOME/.config/BraveSoftware/Brave-Browser/Default" "$HOME/MEGA/Linuxrc/BraveSettings/"
     echo "Settings successfully saved!"
 }
 
