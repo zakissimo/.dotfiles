54dcfdb91c147a84fed433dc5879c9d088600737
zakissimo <zakaria.habri@gmail.com>
2022-02-09 11:52:23 +0100

    update

--- InstallationScripts/deploy.sh
+++ InstallationScripts/deploy.sh
@@ -13,7 +13,7 @@ cd .. && sudo rm -rf yay
 
 yay -S picom-jonaburg-git megasync-bin brave-bin nerd-fonts-cascadia-code ttf-impallari-lobster-font visual-studio-code-bin xkblayout-state pa-applet-git
 
-sudo pip install pynvim requests black autopep8 pylint
+sudo pip install pynvim requests black autopep8 pylint psutil
 
 sudo npm install neovim
 
