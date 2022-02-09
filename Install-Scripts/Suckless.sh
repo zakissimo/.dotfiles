d111351955bca98de45f09303cae034716873f9f
zakissimo <zakaria.habri@gmail.com>
2022-02-09 19:02:52 +0100

    dmenu install script

--- Install-Scripts/Suckless.sh
+++ Install-Scripts/Suckless.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-cd "$HOME" || echo "Can't cd home!" && exit
+cd "$HOME" || echo "Can't cd home!"
 
 git clone https://github.com/bakkeby/dmenu-flexipatch
 
 rm "$HOME"/dmenu-flexipatch/config.def.h
 cp "$HOME"/.dotfiles/suckless/dmenu/config.h "$HOME"/dmenu-flexipatch/config.h
 
-cd "$HOME"/dmenu-flexipatch || echo "Can't cd in dmenu folder!" && exit
+cd "$HOME"/dmenu-flexipatch || echo "Can't cd in dmenu folder!"
 
 sudo make clean install
