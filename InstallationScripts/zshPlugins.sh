8c0add9fed614bbe757b73843e474eb1a7464a2a
zakissimo <zakaria.habri@gmail.com>
2022-02-09 11:42:32 +0100

    update

--- InstallationScripts/zshPlugins.sh
+++ InstallationScripts/zshPlugins.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+mv "$HOME"/.oh-my-zsh "$HOME"/.config/oh-my-zsh
+
 git clone https://github.com/agkozak/zsh-z "$HOME"/.config/oh-my-zsh/plugins/zsh-z
 git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME"/.config/oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME"/.config/oh-my-zsh/plugins/zsh-syntax-highlighting
