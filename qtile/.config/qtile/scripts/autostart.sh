c336d3b71fe16b0574df06d341af73ac7ec8cf70
zakissimo <zakaria.habri@gmail.com>
2022-02-16 19:46:16 +0100

    clean up

--- qtile/.config/qtile/scripts/autostart.sh
+++ qtile/.config/qtile/scripts/autostart.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-key &
 wal -R &
 picom -b &
 
 nm-applet &
 megasync &
 gxkb &
+key &
 "$HOME/MEGA/Apps/g14_alert" &
 
 blue=$(systemctl list-units --all -t service --full --no-legend "bluetooth.service")
