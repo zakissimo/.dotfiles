bb796d6b8df8a37f282af1d943c51ac3a712a4da
zakissimo <zakaria.habri@gmail.com>
2022-03-08 14:53:20 +0100

    New wm bspwm config and stuff

---
+++ bspwm-sxhkd-polybar/.config/bspwm/autostart.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+wal -R &
+picom -b &
+
+nm-applet &
+megasync &
+# gxkb &
+
+"$HOME/MEGA/Apps/g14_alert" &
+
+if [ -d "/proc/acpi/button/lid" ]; then
+	cbatticon -u 20 -i notification -c "poweroff" -l 15 -r 3 &
+fi
+
+blue=$(systemctl list-units --all -t service --full --no-legend "bluetooth.service")
+if [[ $blue ]]; then
+	blueman-applet &
+fi
+
+sleep 1
+
+key &
