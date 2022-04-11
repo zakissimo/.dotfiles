1bf45ea71f798d1cff41764142b2db46c157367f
zakissimo <zakaria.habri@gmail.com>
2022-04-11 18:43:28 +0200

    Smart mappings for brackets [lol] {hehe}

--- neovim/.config/nvim/lua/binds.lua
+++ neovim/.config/nvim/lua/binds.lua
@@ -2,6 +2,7 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = " "
+
 map("n", "<C-s>", ":w<CR>", opts)
 map("n", "<C-n>", ":tabnew<CR>", opts)
 map("n", "<A-w>", ":tabclose<CR>", opts)
@@ -21,10 +22,10 @@ map("v", "s", ":s///g<Left><Left><Left>", { noremap = true })
 map("v", "<", "<gv", opts)
 map("v", ">", ">gv", opts)
 
-map("n", "<M-j>", ":resize -2<CR>", opts)
-map("n", "<M-k>", ":resize +2<CR>", opts)
-map("n", "<M-h>", ":vertical resize -2<CR>", opts)
-map("n", "<M-l>", ":vertical resize +2<CR>", opts)
+map("n", "<A-j>", ":resize -2<CR>", opts)
+map("n", "<A-k>", ":resize +2<CR>", opts)
+map("n", "<A-h>", ":vertical resize -2<CR>", opts)
+map("n", "<A-l>", ":vertical resize +2<CR>", opts)
 
 map("n", "<A-,>", ":bprevious<CR>", opts)
 map("n", "<A-;>", ":bnext<CR>", opts)
@@ -37,9 +38,9 @@ map("n", "<Leader>fl", ":FzfLua live_grep<CR>", opts)
 map("n", "<Leader>o", ":lua require'fzf-lua'.files({ cwd='~' })<CR>", opts)
 map("n", "<Leader>ff", ":FzfLua files<CR>", opts)
 
-map("n", "<M-p>", ":lua require('harpoon.mark').add_file()<CR>", opts)
-map("n", "<M-o>", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
-map("n", "<M-j>", ":lua require('harpoon.ui').nav_file(1)<CR>", opts)
-map("n", "<M-k>", ":lua require('harpoon.ui').nav_file(2)<CR>", opts)
-map("n", "<M-l>", ":lua require('harpoon.ui').nav_file(3)<CR>", opts)
-map("n", "<M-m>", ":lua require('harpoon.ui').nav_file(4)<CR>", opts)
+map("n", "<A-p>", ":lua require('harpoon.mark').add_file()<CR>", opts)
+map("n", "<A-o>", ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
+map("n", "<A-y>", ":lua require('harpoon.ui').nav_file(1)<CR>", opts)
+map("n", "<A-u>", ":lua require('harpoon.ui').nav_file(2)<CR>", opts)
+map("n", "<A-i>", ":lua require('harpoon.ui').nav_file(3)<CR>", opts)
+-- map("n", "<A-o>", ":lua require('harpoon.ui').nav_file(4)<CR>", opts)
--- neovim/.config/nvim/lua/lsp/handlers.lua
+++ neovim/.config/nvim/lua/lsp/handlers.lua
@@ -60,7 +60,7 @@ local function lsp_keymaps(bufnr)
 	local opts = { noremap = true, silent = true }
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gK", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
