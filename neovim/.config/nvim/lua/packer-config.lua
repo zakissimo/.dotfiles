568ba6dce8fc476bb9422ea495f81294c933d9f7
zakissimo <zakaria.habri@gmail.com>
2022-04-11 02:59:37 +0200

    lel

--- neovim/.config/nvim/lua/packer-config.lua
+++ neovim/.config/nvim/lua/packer-config.lua
@@ -65,6 +65,7 @@ return require("packer").startup(function(use)
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use({ "ThePrimeagen/harpoon" })
 
 	use("windwp/nvim-autopairs")
 	use({
