a514f74073dab1d3f7cd5b4dc12585b29224dd73
zakissimo <zakaria.habri@gmail.com>
2022-04-24 06:40:34 +0200

    adding vimv to bins

--- Install-Scripts/deploy.sh
+++ Install-Scripts/deploy.sh
@@ -21,7 +21,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si
 cd .. && rm -rf yay
 
-yay -S --noconfirm picom-jonaburg-git megasync-bin nerd-fonts-cascadia-code ttf-impallari-lobster-font nerd-fonts-jetbrains-mono xkblayout-state stylua dracula-gtk-theme dracula-icons-git dracula-cursors-git notion-app-enhanced devour busted vim-vader-git ly lf atool vimv-git i3lock-color nmap-netcat kvantum-qt5-git zeal
+yay -S --noconfirm picom-jonaburg-git megasync-bin nerd-fonts-cascadia-code ttf-impallari-lobster-font nerd-fonts-jetbrains-mono xkblayout-state stylua dracula-gtk-theme dracula-icons-git dracula-cursors-git notion-app-enhanced devour busted vim-vader-git ly lf atool i3lock-color nmap-netcat kvantum-qt5-git zeal
 
 sudo pacman -Sdd --asdeps libvterm
 
