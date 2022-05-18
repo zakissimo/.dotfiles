2e65fe3f4e4a187bba00a4f1145de2ae78653c07
zak <zakaria.habri@gmail.com>
2022-05-18 20:43:35 +0200

    Closing nvim if tree is alone

--- neovim/.config/nvim/lua/tree-config.lua
+++ neovim/.config/nvim/lua/tree-config.lua
@@ -3,3 +3,12 @@ require("nvim-tree").setup({
 		enable = true,
 	},
 })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	nested = true,
+	callback = function()
+		if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+			vim.cmd("quit")
+		end
+	end,
+})
