4fdbe0f9fab3feb6f15fdd252fce5542706aac25
zak <zakaria.habri@gmail.com>
2022-05-18 22:30:12 +0200

    power management for laptop

--- Install-Scripts/laptop-misc.sh
+++ Install-Scripts/laptop-misc.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 if [ -d "/proc/acpi/button/lid" ]; then
-	sudo pacman --noconfirm -S bluez bluez-utils blueman cbatticon
+	sudo pacman --noconfirm -S bluez bluez-utils blueman cbatticon tlp
 	systemctl enable bluetooth.service
+	systemctl enable tlp.service
 else
 	echo "According to my calculations, this machine isn't a laptop. See ya!"
 fi
