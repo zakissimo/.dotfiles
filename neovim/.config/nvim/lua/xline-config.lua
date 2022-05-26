a4fdf1905ef82e3df954f3af107e5a43d1e3d5a9
zak <zakaria.habri@gmail.com>
2022-05-26 13:12:01 +0200

    clean up xline

--- neovim/.config/nvim/lua/xline-config.lua
+++ neovim/.config/nvim/lua/xline-config.lua
@@ -2,6 +2,14 @@ require("lualine").setup({
 	options = {
 		theme = "iceberg_dark",
 		section_separators = { left = "", right = "" },
-		component_separators = "",
+		component_separators = { left = "", right = "" },
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = { "filename" },
+		lualine_x = { "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
 	},
 })
