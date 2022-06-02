27ffb6ea60b99a3dd53a8a6815bfa8dc4ac97b2d
zakissimo <zakaria.habri@gmail.com>
2022-06-02 20:51:38 +0200

    up

--- neovim/.config/nvim/lua/xline-config.lua
+++ neovim/.config/nvim/lua/xline-config.lua
@@ -1,6 +1,6 @@
 require("lualine").setup({
 	options = {
-		theme = "iceberg_dark",
+		-- theme = "iceberg_dark",
 		section_separators = { left = "", right = "" },
 		component_separators = { left = "", right = "" },
 	},
