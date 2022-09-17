dff92811270012c04ea269170d78ca2752b57069
zakissimo <zakaria.habri@gmail.com>
2022-09-17 11:40:05 +0200

    up

---
+++ neovim/.config/nvim/lua/autocmd.lua
@@ -0,0 +1,33 @@
+local set = vim.opt
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	group = vim.api.nvim_create_augroup("TabSpace", { clear = true }),
+	callback = function()
+		local ft = vim.bo.filetype
+		if ft == "css" or ft == "html" then
+			set.tabstop = 2
+			set.shiftwidth = 2
+			set.expandtab = true
+		else
+			set.tabstop = 4
+			set.shiftwidth = 4
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	group = vim.api.nvim_create_augroup("YankHl", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
+})
+
+vim.api.nvim_create_autocmd(
+	{ "CursorMoved", "CursorHold", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost", "TabClosed" },
+	{
+		group = vim.api.nvim_create_augroup("RefreshWinBar", { clear = true }),
+		callback = function()
+			require("winbar").get_winbar()
+		end,
+	}
+)
---
+++ neovim/.config/nvim/lua/harpoon.lua
@@ -0,0 +1,8 @@
+require("harpoon").setup({
+	global_settings = {
+		-- set marks specific to each git branch inside git repository
+		save_on_toggle = true,
+		mark_branch = true,
+	},
+})
+
