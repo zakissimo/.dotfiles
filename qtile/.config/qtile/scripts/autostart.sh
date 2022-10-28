dc08830e14c8f834b01e7ba5422ba39b607e5b4e
zakissimo <zakaria.habri@gmail.com>
2022-10-28 13:47:10 +0200

    up

--- qtile/.config/qtile/scripts/autostart.sh
+++ qtile/.config/qtile/scripts/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wal -R &
+wal -R
 picom &
 
 nm-applet &
@@ -13,7 +13,5 @@ sxhkd &
 # asusctl led-mode static -c ffffff
 
 cbatticon -u 20 -i notification -c "poweroff" -l 15 -r 3 &
-
-input-remapper-control --command autoload
-
 key
+xrdb ~/.Xresources
