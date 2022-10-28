ab84340052b303c8dafac7e6fc640dc2274d4e6f
zakissimo <zakaria.habri@gmail.com>
2022-10-28 16:54:30 +0200

    up

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -130,7 +130,7 @@ const int boxdraw_braille = 0;
 static int bellvolume = 0;
 
 /* default TERM value */
-char *termname = "st-256color";
+char *termname = "xterm-256color";
 
 /*
  * spaces per tab
