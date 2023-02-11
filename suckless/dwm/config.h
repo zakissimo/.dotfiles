3984198cf0daa97994f7d6437db2c2809b406c2c
zakissimo <zakaria.habri@gmail.com>
2023-02-11 21:02:22 +0100

    dwm logos change

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -22,7 +22,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "ﱄ" };
+static const char *tags[] = { "󰈹", "", "", "󱜏" };
 
 static const Rule rules[] = {
 	/* xprop(1):
