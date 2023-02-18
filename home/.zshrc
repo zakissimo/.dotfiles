20c9b4aecdba841a6136effaec9f32355fabb2ed
zakissimo <zakaria.habri@gmail.com>
2023-02-18 15:56:15 +0100

    up

--- home/.zshrc
+++ home/.zshrc
@@ -8,8 +8,6 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
     source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-xrdb ~/.Xresources
-
 if [[ -f "$HOME/zsh/keys.zsh" ]]; then
 	source "$HOME/zsh/keys.zsh"
 fi
--- suckless/st-0.9/config.h
+++ suckless/st-0.9/config.h
@@ -115,35 +115,16 @@ float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
+	// TokyoNight storm
+	"#1D202F", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7", "#7dcfff",
+	"#a9b1d6", "#414868", "#f7768e", "#9ece6a", "#e0af68", "#7aa2f7", "#bb9af7",
 	[255] = 0,
-
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#c0caf5",
+	"#1D202F",
+	"#1D202F",
+	"#1D202F",
 };
-
 
 /*
  * Default colors (colorname index)
