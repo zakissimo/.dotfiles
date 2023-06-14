47eb236ca81f3160d10d69260f28a835470ff7f2
zakissimo <zakaria.habri@gmail.com>
2023-06-14 18:53:48 +0200

    Update deploy to be distro agnosticish and use nix-env

--- Install-Scripts/deploy.sh
+++ Install-Scripts/deploy.sh
@@ -1,31 +1,53 @@
 #!/usr/bin/env bash
 
-sudo apt update && sudo apt upgrade
+set -ex
 
-apps=(
-    curl
-    delta
-    exa
-    fuse
-    fzf
-    git
-    lua
-    nodejs
-    npm
-    ripgrep
-    ssh
-    stow
-    tmux
-    zsh
+echo "Define root password: "
+passwd
+
+command -v apt >/dev/null 2>&1 && apt update && apt upgrade && INSTALL="apt install -y --no-install-recommends"
+command -v apk >/dev/null 2>&1 && apk update && apk upgrade && INSTALL="apk add -y"
+command -v pacman >/dev/null 2>&1 && pacman update && pacman upgrade && INSTALL="pacman -S --noconfirm"
+
+apps=(curl sudo)
+
+function install {
+    for app in $1; do
+    which "$app" \
+        || $2 "$app"
+    done
+}
+
+install "${apps[@]}" "$INSTALL"
+
+echo "Enter your username: "
+read -r USER
+useradd -m -G sudo -s /bin/bash "$USER"
+su - "$USER"
+echo "Define user password: "
+passwd
+
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+nixpkgs=(
+    nixpkgs.curl
+    nixpkgs.delta
+    nixpkgs.exa
+    nixpkgs.fuse
+    nixpkgs.fzf
+    nixpkgs.git
+    nixpkgs.lazygit
+    nixpkgs.lua
+    nixpkgs.nodejs
+    nixpkgs.npm
+    nixpkgs.nvim
+    nixpkgs.ripgrep
+    nixpkgs.ssh
+    nixpkgs.stow
+    nixpkgs.tmux
+    nixpkgs.zsh
 )
 
-for app in "${apps[@]}"; do
-    which "$app" \
-        || DEBIAN_FRONTEND=noninteractive \
-        sudo apt install -y \
-        --no-install-recommends \
-        "$app" \
-        && rm -rf /var/lib/apt/lists/*
-done
+install "${nixpkgs[@]}" "nix-env -iA"
 
 sudo chsh -s "$(which zsh)" "$(whoami)"
