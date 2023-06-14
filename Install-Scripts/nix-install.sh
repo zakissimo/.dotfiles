910e2ba83290a6c40cb8e82b75dfda022e5f9d50
zakissimo <zakaria.habri@gmail.com>
2023-06-14 22:48:12 +0200

    tweaks

--- Install-Scripts/nix-install.sh
+++ Install-Scripts/nix-install.sh
@@ -35,6 +35,6 @@ INSTALL="nix-env -iA"
 
 install "${nixpkgs[@]}"
 
-git clone https://github.com/zakissimo/.dotfiles "$HOME/" \
+cd && git clone https://github.com/zakissimo/.dotfiles \
     && cd .dotfiles \
     && git remote set-url origin git@github:zakissimo/.dotfiles
