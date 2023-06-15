967ec40c72eafc178ea820123a73865bc48b2084
zakissimo <zakaria.habri@gmail.com>
2023-06-15 13:46:15 +0200

    Ask user if setting a pass and creating a new user is needed

--- Install-Scripts/deploy.sh
+++ Install-Scripts/deploy.sh
@@ -2,8 +2,16 @@
 
 set -xe
 
-echo "Define root password: "
-passwd
+read -pr "Define root password? [Y/n]" ROOT_PASSWD
+case "$ROOT_PASSWD" in
+    [nN])
+        echo "Skipping root password"
+        ;;
+    *)
+        echo "Define root password:"
+        passwd
+        ;;
+esac
 
 command -v apt >/dev/null 2>&1 && apt update -y && INSTALL="apt install -y" && SUDO_GRP="sudo"
 command -v pacman >/dev/null 2>&1 && pacman -Syyu --noconfirm && INSTALL="pacman -S --noconfirm" && SUDO_GRP="wheel"
@@ -19,11 +27,33 @@ function install {
 
 install "${apps[@]}"
 
-echo "Enter your username: "
-read -r NEW_USER
-useradd -m -G "$SUDO_GRP" -s /bin/bash "$NEW_USER"
-echo "Define user password: "
-passwd "$NEW_USER"
+read -pr "Use existing user? [Y/n]" EXISTING_USER
+case "$EXISTING_USER" in
+    [nN])
+        read -pr "Enter new username:" NEW_USER
+        useradd -m -G "$SUDO_GRP" -s /bin/bash "$NEW_USER"
+        read -pr "Define user password? [Y/n]" USER_PASSWD
+        case "$USER_PASSWD" in
+            [nN])
+                echo "Skipping root password"
+                ;;
+            *)
+                echo "Define root password:"
+                passwd "$NEW_USER"
+        ;;
+        esac
+        ;;
+    *)
+        while true; do
+        read -pr "Enter existing username:" NEW_USER
+        if id "$NEW_USER" &>/dev/null; then
+            echo "User $NEW_USER exists."
+            break
+        else
+            echo "User $NEW_USER does not exist. Please try again."
+        fi
+        done
+esac
 
 cp /.dotfiles/Install-Scripts/nix-install.sh /home/"$NEW_USER"
 chown "$NEW_USER":"$NEW_USER" /home/"$NEW_USER"/nix-install.sh
