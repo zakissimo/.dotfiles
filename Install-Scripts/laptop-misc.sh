6defda49a93d9ddd2568e8fa0aea1256c8915546
zakissimo <zakaria.habri@gmail.com>
2024-03-18 11:05:08 +0100

    Clean up

--- Install-Scripts/laptop-misc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -d "/proc/acpi/button/lid" ]; then
-	sudo pacman --noconfirm -S bluez bluez-utils blueman cbatticon tlp
-	systemctl enable bluetooth.service
-	systemctl enable tlp.service
-else
-	echo "According to my calculations, this machine isn't a laptop. See ya!"
-fi
