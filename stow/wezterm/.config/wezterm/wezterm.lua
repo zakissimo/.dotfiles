206fad3c651b11696d69c7c9f34e55374534be87
Zakaria Habri <zakaria.habri@gmail.com>
2024-12-14 20:43:58 +0100

    mod: tweaks

--- stow/wezterm/.config/wezterm/wezterm.lua
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local act = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 config.audible_bell = "Disabled"
@@ -18,20 +18,20 @@ config.enable_wayland = false
 config.font_size = 11
 config.bidi_enabled = true
 config.font = wezterm.font_with_fallback({
-    "FiraCode Nerd Font",
-    "Noto Sans Mono CJK SC",
-    "Noto Naskh Arabic",
+  "FiraCode Nerd Font",
+  "Noto Sans Mono CJK SC",
+  "Noto Naskh Arabic",
 })
 config.warn_about_missing_glyphs = true
-config.window_background_opacity = 0.75
+config.window_background_opacity = 1
 
 config.enable_scroll_bar = false
 
 config.window_padding = {
-    left = 5,
-    right = 0,
-    top = 21,
-    bottom = 0,
+  left = 15,
+  right = 15,
+  top = 15,
+  bottom = 0,
 }
 
 config.freetype_load_target = "HorizontalLcd"
@@ -44,93 +44,96 @@ config.default_cursor_style = "SteadyBlock"
 config.hide_mouse_cursor_when_typing = false
 config.window_close_confirmation = "NeverPrompt"
 
-config.leader = { key = "Space", mods = "CTRL", timeout_milliseconds = 500 }
+config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 500 }
 
 config.keys = {
-    {
-        key = "w",
-        mods = "ALT",
-        action = act.CloseCurrentTab({ confirm = false }),
-    },
-    {
-        key = "l",
-        mods = "ALT",
-        action = act.ActivateTabRelative(1),
-    },
-    {
-        key = "h",
-        mods = "ALT",
-        action = act.ActivateTabRelative(-1),
-    },
-    {
-        key = "n",
-        mods = "ALT",
-        action = act.SpawnTab("DefaultDomain"),
-    },
-    {
-        key = "[",
-        mods = "LEADER",
-        action = act.ActivateCopyMode,
-    },
-    {
-        key = "Enter",
-        mods = "ALT",
-        action = act.DisableDefaultAssignment,
-    },
-    {
-        key = "LeftArrow",
-        mods = "OPT",
-        action = act({ SendString = "\x1bb" }),
-    },
-    {
-        key = "RightArrow",
-        mods = "OPT",
-        action = act({ SendString = "\x1bf" }),
-    },
+  {
+    key = "w",
+    mods = "ALT",
+    action = act.CloseCurrentTab({ confirm = false }),
+  },
+  {
+    key = "l",
+    mods = "ALT",
+    action = act.ActivateTabRelative(1),
+  },
+  {
+    key = "h",
+    mods = "ALT",
+    action = act.ActivateTabRelative(-1),
+  },
+  {
+    key = "n",
+    mods = "ALT",
+    action = act.SpawnTab("DefaultDomain"),
+  },
+  {
+    key = "Space",
+    mods = "LEADER",
+    action = act.ActivateCopyMode,
+  },
+  {
+    key = "Enter",
+    mods = "ALT",
+    action = act.DisableDefaultAssignment,
+  },
+  {
+    key = "LeftArrow",
+    mods = "OPT",
+    action = act({ SendString = "\x1bb" }),
+  },
+  {
+    key = "RightArrow",
+    mods = "OPT",
+    action = act({ SendString = "\x1bf" }),
+  },
 }
 
 config.colors = {
-    tab_bar = {
-        background = "none",
+  selection_fg = "black",
+  selection_bg = "#fffacd",
 
-        active_tab = {
-            bg_color = "#2b2042",
-            fg_color = "#c0c0c0",
+  tab_bar = {
+    background = "none",
 
-            intensity = "Half",
-            underline = "None",
-            italic = false,
-            strikethrough = false,
-        },
+    active_tab = {
+      bg_color = "#2b2042",
+      fg_color = "#c0c0c0",
 
-        inactive_tab = {
-            bg_color = "#1b1032",
-            fg_color = "#808080",
-        },
-
-        inactive_tab_hover = {
-            bg_color = "#3b3052",
-            fg_color = "#909090",
-            italic = true,
-        },
-
-        new_tab = {
-            bg_color = "None",
-            fg_color = "None",
-        },
-
-        new_tab_hover = {
-            bg_color = "#3b3052",
-            fg_color = "#909090",
-            italic = false,
-        },
+      intensity = "Half",
+      underline = "None",
+      italic = false,
+      strikethrough = false,
     },
+
+    inactive_tab = {
+      bg_color = "#1b1032",
+      fg_color = "#808080",
+    },
+
+    inactive_tab_hover = {
+      bg_color = "#3b3052",
+      fg_color = "#909090",
+      italic = true,
+    },
+
+    new_tab = {
+      bg_color = "None",
+      fg_color = "None",
+    },
+
+    new_tab_hover = {
+      bg_color = "#3b3052",
+      fg_color = "#909090",
+      italic = false,
+    },
+  },
 }
 
 wezterm.on("format-tab-title", function(tab)
-    return {
-        { Text = " " .. tab.active_pane.title .. " " },
-    }
+  return {
+    { Text = " " .. tab.active_pane.title .. " " },
+  }
 end)
 
 return config
